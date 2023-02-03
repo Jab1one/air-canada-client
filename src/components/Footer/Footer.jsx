f9128cfa5bf87a48bc128876fb02cda61f9208ef
Sagar Suresh <saagarsuresh92@gmail.com>
2023-02-02 23:06:14 -0500

    Add Header, Body and Footer components to homepage.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import "./Footer.scss"
+import footer from "../../assets/images/Footer.svg"
+
+export default function Footer(){
+    return(
+        <img src={footer} className="footer"/>
+    )
+}
